82cf5b9e6aedbe52ac158cb3780dcb4ec9b9b8ff
Victor <victorlaucas@gmail.com>
2018-01-30 19:00:57 -0700

    Creating the App Component

--- freelance-camp-fe/app/app.component.js
+++ freelance-camp-fe/app/app.component.js
@@ -7,16 +7,17 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
-var HomepageComponent = (function () {
-    function HomepageComponent() {
+var AppComponent = (function () {
+    function AppComponent() {
     }
-    HomepageComponent = __decorate([
+    AppComponent = __decorate([
         core_1.Component({
-            selector: 'main-app',
-            template: '<h1>Freelance Bootcamp Homepage</h1>'
+            moduleId: module.id,
+            selector: 'app',
+            template: '<h1>Navigation</h1>'
         })
-    ], HomepageComponent);
-    return HomepageComponent;
+    ], AppComponent);
+    return AppComponent;
 }());
-exports.HomepageComponent = HomepageComponent;
-//# sourceMappingURL=homepage.component.js.map
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.component.js.map
--- freelance-camp-fe/app/app.module.js
+++ freelance-camp-fe/app/app.module.js
@@ -8,7 +8,8 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
-var homepage_component_1 = require("./homepage.component");
+var app_component_1 = require("./app.component");
+var homepage_component_1 = require("./homepage/homepage.component");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -18,10 +19,11 @@ var AppModule = (function () {
                 platform_browser_1.BrowserModule
             ],
             declarations: [
+                app_component_1.AppComponent,
                 homepage_component_1.HomepageComponent
             ],
             bootstrap: [
-                homepage_component_1.HomepageComponent
+                app_component_1.AppComponent
             ]
         })
     ], AppModule);
