36172d64f26f1106cbd2b0fa479a55afc567f0a0
Victor <victorlaucas@gmail.com>
2018-01-30 21:59:51 -0700

    Displayed component data from view

--- freelance-camp-fe/app/documents/documents.component.js
+++ freelance-camp-fe/app/documents/documents.component.js
@@ -9,6 +9,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var DocumentsComponent = (function () {
     function DocumentsComponent() {
+        this.pageTitle = "Document Dashboard";
         this.documents = [
             {
                 title: "My First Doc",
