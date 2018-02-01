f4c6d01d694bc2d8b7352b308c57a35cf4b1e78d
Victor <victorlaucas@gmail.com>
2018-01-31 22:56:36 -0700

    Created proposal show component and set up route based on id

---
+++ freelance-camp-fe/app/proposal/proposal-show.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'proposal-show.component',
+  templateUrl: 'proposal-show.component.html'
+})
+export class ProposalShowComponent {}
