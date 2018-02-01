fa8cb2bc501fe26e23b7a79bc3d49edc06cbc7e0
Victor <victorlaucas@gmail.com>
2018-01-31 21:43:44 -0700

    Fixed proposal model. Data types were incorrect

--- freelance-camp-fe/app/proposal/proposal.ts
+++ freelance-camp-fe/app/proposal/proposal.ts
@@ -4,9 +4,9 @@ export class Proposal {
     public customer?: string,
     public portfolio_url: string = 'http://',
     public tools?: string,
-    public estimated_hours?: string,
-    public hourly_rate?: string,
-    public weeks_to_complete?: string,
+    public estimated_hours?: number,
+    public hourly_rate?: number,
+    public weeks_to_complete?: number,
     public client_email?: string
   ) {}
 }
