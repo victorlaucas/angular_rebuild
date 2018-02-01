63fda834d846cf51a01849d6a17a4173adda7884
Victor <victorlaucas@gmail.com>
2018-01-31 21:44:50 -0700

    created mock proposal data and exported proposals to view component to be populated

--- freelance-camp-fe/app/proposal/proposal-list.component.js
+++ freelance-camp-fe/app/proposal/proposal-list.component.js
@@ -7,8 +7,17 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var proposal_1 = require("./proposal");
 var ProposalListComponent = (function () {
     function ProposalListComponent() {
+        this.proposalOne = new proposal_1.Proposal(15, "ABC Company", "http://portfolio.jordanhudgens.com", 'Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com');
+        this.proposalTwo = new proposal_1.Proposal(99, "XYZ Company", "http://portfolio.jordanhudgens.com", 'Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com');
+        this.proposalThree = new proposal_1.Proposal(300, "Something Company", "http://portfolio.jordanhudgens.com", 'Ruby on Rails', 150, 120, 15, 'jordan@devcamp.com');
+        this.proposals = [
+            this.proposalOne,
+            this.proposalTwo,
+            this.proposalThree
+        ];
     }
     ProposalListComponent = __decorate([
         core_1.Component({
