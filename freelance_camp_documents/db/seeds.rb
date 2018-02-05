de08282fd088aed084fd3367603267613902e860
Victor <victorlaucas@gmail.com>
2018-02-04 22:57:42 -0700

    Added new data to documents

--- freelance_camp_documents/db/seeds.rb
+++ freelance_camp_documents/db/seeds.rb
@@ -5,4 +5,4 @@
   file_url: "https://docs.google.com/document/d/1UUgAob6ZpivgkgCbMD84JMMRc1flrzCPKybtQoAfASo/edit?usp=sharing",
   image_url: 'https://s3.amazonaws.com/devcamp-static/images/freelance-img.jpg'
   )
-end 
+end
