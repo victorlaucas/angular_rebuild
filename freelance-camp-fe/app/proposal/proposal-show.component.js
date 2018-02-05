cf386295a993799e8e3d8bbb7d766b9defe4d6b2
Victor <victorlaucas@gmail.com>
2018-02-05 13:53:58 -0700

    Implemented api data on proposal show page

--- freelance-camp-fe/app/proposal/proposal-show.component.js
+++ freelance-camp-fe/app/proposal/proposal-show.component.js
@@ -11,24 +11,38 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var router_1 = require("@angular/router");
+var http_1 = require("@angular/http");
+var proposal_service_1 = require("./proposal.service");
+var proposal_1 = require("./proposal");
 var ProposalShowComponent = (function () {
-    function ProposalShowComponent(route) {
+    function ProposalShowComponent(route, proposalService, http) {
         this.route = route;
+        this.proposalService = proposalService;
+        this.http = http;
     }
     ProposalShowComponent.prototype.ngOnInit = function () {
         var _this = this;
-        this.routeId = this.route.params.subscribe(function (params) {
-            _this.id = +params['id'];
+        var proposalRequest = this.route.params
+            .flatMap(function (params) {
+            return _this.proposalService.getProposal(+params['id']);
         });
+        proposalRequest.subscribe(function (response) { return _this.proposal = response.json(); });
     };
+    __decorate([
+        core_1.Input(),
+        __metadata("design:type", proposal_1.Proposal)
+    ], ProposalShowComponent.prototype, "proposal", void 0);
     ProposalShowComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
             selector: 'proposal-show.component',
             templateUrl: 'proposal-show.component.html',
-            styleUrls: ['proposal-show.component.css']
+            styleUrls: ['proposal-show.component.css'],
+            providers: [proposal_service_1.ProposalService]
         }),
-        __metadata("design:paramtypes", [router_1.ActivatedRoute])
+        __metadata("design:paramtypes", [router_1.ActivatedRoute,
+            proposal_service_1.ProposalService,
+            http_1.Http])
     ], ProposalShowComponent);
     return ProposalShowComponent;
 }());
--- freelance-camp-fe/app/proposal/proposal-show.component.ts
+++ freelance-camp-fe/app/proposal/proposal-show.component.ts
@@ -1,26 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import { ProposalService } from './proposal.service';
 import { Proposal } from './proposal';
 
 @Component({
   moduleId: module.id,
   selector: 'proposal-show.component',
   templateUrl: 'proposal-show.component.html',
-  styleUrls: ['proposal-show.component.css']
+  styleUrls: ['proposal-show.component.css'],
+  providers: [ProposalService]
 })
 export class ProposalShowComponent implements OnInit {
-  id: number;
-  routeId: any;
-
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private proposalService: ProposalService,
+    private http: Http
   ){}
 
+  @Input()
+  proposal: Proposal;
+
   ngOnInit(): void {
-    this.routeId = this.route.params.subscribe(
-      params => {
-        this.id = +params['id']
-      }
-    )
+    let proposalRequest = this.route.params
+       .flatMap((params: Params) => 
+         this.proposalService.getProposal(+params['id']));
+    proposalRequest.subscribe(response => this.proposal = response.json());
   }
 }
